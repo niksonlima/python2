96dfae5fc2aaf77da29be15898ed26f26df89cb5
niksonlima <niksonpalmeiraslima@gmail.com>
2024-09-12 12:22:14 -0300

    Create notamedia2.py

---
+++ notamedia2.py
@@ -0,0 +1,8 @@
+print('Coloque suas notas')
+n1 = float(input("nota 1: "))
+n2 = float(input("nota 2: "))
+n3 = float(input("nota 3: "))
+n4 = float(input("nota 4: "))
+n5 = float(input("nota 5: "))
+media = (n1+n2+n3+n4+n5) / 5
+print(media)
