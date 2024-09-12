ed2feeae4c20d921975c0ecd7ba938c82e27d0bb
niksonlima <niksonpalmeiraslima@gmail.com>
2024-09-12 12:21:20 -0300

    Create notamedia1.py

---
+++ notamedia1.py
@@ -0,0 +1,8 @@
+nota = int(input('Digite uma nota: '))
+nota2 = int(input('Digite uma nota2: '))
+nota3 = int(input('Digite uma nota3: '))
+nota4 = int(input('Digite uma nota4: '))
+print(nota, nota2, nota3, nota4)
+media = (nota + nota2 + nota3 + nota4) / 4
+media1 = (nota/4 + nota2/4 + nota3/4 + nota4/4)
+print(media,media1)
